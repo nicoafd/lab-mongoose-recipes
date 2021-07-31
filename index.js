56d6f2d56d0402f0a7306c80a4fd5f307c582888
nicoafd <dottinick@gmail.com>
2021-07-31 13:35:51 +0200

    Iteration 4 COMPLETE

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
+    console.log("1. All Deleted");
     // create
-    //return Recipe.create( { title: "Pimientos de Padron", level: "Easy Peasy", ingredients: ["Salt", "Pimientos de Padron", "Olive Oil"], cuisine: "Spanish", dishType: "snack", duration: 15, creator: "Kick" } )
+    return Recipe.create({
+      title: "Pimientos de Padron",
+      level: "Easy Peasy",
+      ingredients: ["Salt", "Pimientos de Padron", "Olive Oil"],
+      cuisine: "Spanish",
+      dishType: "snack",
+      duration: 15,
+      creator: "Kick",
+    });
     // Run your code here, after you have insured that the connection was made
+  })
+  .then((response) => {
+    console.log("2. Showing just the title: ", response.title);
 
+    return Recipe.insertMany(data);
   })
-  .then(response => {
-    console.log("1. Showing just the title: ", response.title)
+  .then((response) => {
+    console.log("3. Recipes added correctly", response);
+    // >>>? how to print titles for iteration 3
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }, 
+      { new: true }
+    );
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((response) => {
+    console.log(
+      "4. You are a goddam Success!! Your Recipe has been Updated!!!", response
+    );
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
